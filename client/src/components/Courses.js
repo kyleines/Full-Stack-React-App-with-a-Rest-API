17b7715a68e8a9d655b8d48f60fa9020c2e88453
kyleines <kyleines@gmail.com>
2021-07-07 16:31:37 -0500

    update Courses component with a 'new course' button

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -3,14 +3,14 @@ import axios from "axios";
 
 const Courses = () => {
 
-    const [courses, setCourses] = useState([]);
+    const [coursesArray, setCoursesArray] = useState([]);
 
     useEffect(() => {
         axios.get("http://localhost:5000/api/courses")
-            .then(res => setCourses(res.data))
+            .then(res => setCoursesArray(res.data))
     }, []);
 
-    let component = courses.map((course, index) => (
+    let courses = coursesArray.map((course, index) => (
         <a key={index} className="course--module course--link" href="course-detail.html">
             <h2 className="course--label">Course</h2>
             <h3 className="course--title">{course.title}</h3>
@@ -19,7 +19,18 @@ const Courses = () => {
 
     return (
         <div className="wrap main--grid">
-            {component}
+
+            {/* list of courses */}
+            {courses}
+
+            {/* new course button */}
+            <a className="course--module course--add--module" href="create-course.html">
+                <span className="course--add--title">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+                    New Course
+                </span>
+            </a>
         </div>
     );
 }
