5a51c09e177ee222bf0133b01f270a3bd195ffd7
kyleines <kyleines@gmail.com>
2021-07-10 17:28:17 -0500

    add comments to Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,33 @@
-import React from "react"
 
+// import modules
+import React, {useContext} from "react"
+import Context from "../Context";
+
+// renders header on all pages
 const Header = () => {
+
+    // data
+    const context = useContext(Context.AppContext);
+    const signedIn = context.authedUser;
+
     return (
         <header>
             <div className="wrap header--flex">
                 <h1 className="header--logo"><a href="/">Courses</a></h1>
                 <nav>
-                    <ul className="header--signedout">
-                        <li><a href="sign-up.html">Sign Up</a></li>
-                        <li><a href="sign-in.html">Sign In</a></li>
-                    </ul>
+                    {/* if authenticated, displays welcome message to the user and a sign-out button */}
+                    {signedIn ? (
+                        <ul className="header--signedin">
+                            <li>Welcome, {`${signedIn.firstName} ${signedIn.lastName}`}!</li>
+                            <li><a href="/signout">Sign Out</a></li>
+                        </ul>
+                    ) : (
+                        // if not authenticated, displays Sign Up and Sign In buttons
+                        <ul className="header--signedout">
+                            <li><a href="/signup">Sign Up</a></li>
+                            <li><a href="/signin">Sign In</a></li>
+                        </ul>
+                    )}
                 </nav>
             </div>
         </header>
