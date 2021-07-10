7003d0b009037ae2b30abafc3c32652ff43a9fed
kyleines <kyleines@gmail.com>
2021-07-10 17:17:42 -0500

    add comments to UnhandledError.js

--- client/src/components/UnhandledError.js
+++ client/src/components/UnhandledError.js
@@ -1,11 +1,16 @@
-import React from "react";
 
+// import modules
+import React from "react";
+import {Link} from "react-router-dom";
+
+// renders for returned status code 500
 const UnhandledError = () => {
     return (
         <main>
             <div className="wrap">
                 <h2>Error</h2>
                 <p>Sorry! We just encountered an unexpected error.</p>
+                <Link className="button button-secondary" to="/">Return to List</Link>
             </div>
         </main>
     );
