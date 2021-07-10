3ec63cbf7b3397f3aca17c2533b3f3ef76bfea43
kyleines <kyleines@gmail.com>
2021-07-10 17:13:44 -0500

    add comments to Forbidden.js

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -1,11 +1,16 @@
-import React from "react";
 
+// import modules
+import React from "react";
+import {Link} from "react-router-dom";
+
+// renders when user authorization checks fail
 const Forbidden = () => {
     return (
         <main>
             <div className="wrap">
                 <h2>Forbidden</h2>
                 <p>Oops! You can't access this page.</p>
+                <Link className="button button-secondary" to="/">Home</Link>
             </div>
         </main>
     );
