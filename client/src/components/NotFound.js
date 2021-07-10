e6cdc50a3491b2cb0704055a341ac22167f9e6e3
kyleines <kyleines@gmail.com>
2021-07-10 17:15:29 -0500

    add comments to NotFound.js

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,11 +1,16 @@
-import React from "react";
 
+// import modules
+import React from "react";
+import {Link} from "react-router-dom";
+
+// renders when url matches no set routes
 const NotFound = () => {
     return (
         <main>
             <div className="wrap">
                 <h2>Not Found</h2>
                 <p>Sorry! We couldn't find the page you're looking for.</p>
+                <Link className="button button-secondary" to="/">Return to List</Link>
             </div>
         </main>
     );
